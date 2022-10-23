726da15a8d9d273c8b331c4be5ee98545ecd80dd
MDhanesh <mgdhanesh98@gmail.com>
2022-10-23 19:46:59 +0530

    link

--- module/forgotModule.js
+++ module/forgotModule.js
@@ -28,8 +28,8 @@ exports.forgot = async (req, res) => {
     const token = jwt.sign(payload, secret, { expiresIn: "15m" });
     console.log(token);
     //const link = `https://localhost:3001/resetpassword/${existuser._id}/${token}`;
-    // const link = `https://crm-app-node.netlify.app/reset/${existuser._id}/${token}`;
-    const link = `https://crm-app-node.netlify.app/resetpassword/${existuser._id}/${token}`;
+    const link = `https://crm-app-node.netlify.app/reset/${existuser._id}/${token}`;
+    //const link = `https://crm-app-node.netlify.app/resetpassword/${existuser._id}/${token}`;
     var transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
