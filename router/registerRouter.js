20d5390fa8d809c1ff50a03a416ae7d3bdd2ee42
MDhanesh <mgdhanesh98@gmail.com>
2022-10-23 20:33:42 +0530

    p

--- router/registerRouter.js
+++ router/registerRouter.js
@@ -7,9 +7,6 @@ router.post("/signup", register.signup);
 router.post("/signin", register.signin);
 router.post("/forgot", changepassword.forgot);
 router.get("/resetpassword/:id/:token", changepassword.resetpassword);
-router.put(
-  "/resetpasswordconfirm/:id/:token",
-  changepassword.resetpasswordconfirm
-);
+router.put("/resetpassword/:id/:token", changepassword.resetpassword);
 
 module.exports = router;
